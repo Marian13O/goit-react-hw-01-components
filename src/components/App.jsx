ba6b8e0cca3728ce74144d2d1e05e4cefbd2dc7e
Marian13O <142900026+Marian13O@users.noreply.github.com>
2024-04-01 14:31:53 +0300

    finish task 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import user from '../data/user.json';
 import Profile from './Profile/Profile';
+
 import data from '../data/data.json';
 import Statistics from './Statistics/Statistics';
+
+import friends from '../data/friends.json';
+import FriendList from './FriendList/FriendList';
 
 export const App = () => {
   return (
@@ -15,6 +19,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
+      <FriendList friends={friends} />;
     </>
   );
 };
